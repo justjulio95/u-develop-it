6a6eefa289e0f434af1d1b4c285edea45fd8e4a5
Julio.S <just.julio95@gmail.com>
2022-05-22 20:22:47 -0400

    standard practices for privacy

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const config = require('config');
+const user = config.get('server.user');
+const pw = config.get('server.pw')
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        //SQL username 'default is root'
+        user: user,
+        // MySQL password
+        password: pw,
+        database:'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
